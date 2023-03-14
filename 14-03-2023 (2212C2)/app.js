da67d202bfafbcce28076703b99443b6f49c7a0b
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-03-14 14:35:08 +0500

    14-03-2023

---
+++ 14-03-2023 (2212C2)/app.js
@@ -0,0 +1,162 @@
+// for(let i=0;i<100;i++){
+
+
+// if(i%2===0){
+//     document.write(i)
+// }
+
+// }
+// let pi=22/7
+
+// document.write(pi.toFixed(4))
+
+
+
+// 2378.52783472347
+
+// let user=+prompt("Please enter price")
+
+// document.write("Your entered price is " +user.toFixed(3))
+
+// let price=+prompt("Please enter price")
+// let disc=+prompt("Please enter discount percentage")
+// document.write("Original Price is " +price+"<br>")
+// let amount=price*(disc/100)
+// document.write("Price after discount " +amount.toFixed(3))
+
+// for(let i=100;i>0;i--){
+// document.write(i+"<br>")
+// }
+
+// for(let j=0;j<100;j++){
+//     document.write(j+"<br>")
+//     }
+
+// for(let i=0;i<=1000;i++){
+//     if(i%3===0){
+//         document.write(i+" Buzz <br>")
+//     }
+//   if(i%5===0){
+//         document.write(i+" Fizz <br>")
+//     }
+//     if(i%3===0&&i%5===0){
+//         document.write(i+" FizzBuzz <br>")
+//     }
+
+// }
+
+
+
+
+//1. Initialization
+//2. Updatation / Condition
+//3. Increment / Decrement
+// let num=+prompt("Please enter a number for table")
+// let start=+prompt("Enter start point")
+// let end=+prompt("Enter end point")
+
+// for(let i=start;i<=end;i++){
+// document.write(num+" * "+i+" = "+(num*i)+"<br>")
+// }
+
+// let num=+prompt("Please enter a number for table")
+// let start=+prompt("Enter start point")
+// let end=+prompt("Enter end point")
+
+// let i=start
+
+// while(i<end){
+//    alert(num+" * "+i+" = "+(num*i)+"<br>")
+//    i++ 
+// }
+
+// let num=+prompt("Please enter a number for table")
+// document.write("num")
+
+// let stdName=prompt("enter your name")
+
+// document.write(stdName)
+
+//STDNAME (Upper Case)
+//stdname  (Lower Case) 
+//stdName  (Camel Case) 
+//std_name (snake case)
+//std-name (Kebab Case)
+//StdName  (Pascal Case)
+
+
+
+
+// let num=+prompt("Please enter a number for table")
+// let start=+prompt("Enter start point")
+// let end=+prompt("Enter end point")
+
+// let i=start
+// do{
+//    alert(num+" * "+i+" = "+(num*i)+"<br>")
+//    i++ 
+// }while(i<end)
+
+// let alphabet=prompt("Enter an alphabet")
+// switch(alphabet){
+//     case "a":
+//         document.write(alphabet +" is Vowel")
+//         break;
+//     case "e":
+//         document.write(alphabet +" is Vowel")
+//         break;
+//     case "i":
+//         document.write(alphabet +" is Vowel")
+//     case "o":
+//         document.write(alphabet +" is Vowel")
+//         break;
+//     case "u":
+//         document.write(alphabet +" is Vowel")
+//         break;
+//     default:
+//         document.write(alphabet +" is CONSONANT")
+//         break;
+// }
+
+// let user=prompt("Enter something")
+// document.write("<p>"+user +"</p>")
+
+// let day=+prompt("Enter day number")
+// switch(day){
+//     case 1:
+//         document.write("Monday")
+//         break;
+//     case 2:
+//         document.write("Tuesday")
+//         break;
+//     case 3:
+//         document.write("Wednesday")
+//         break;
+//     case 4:
+//         document.write("Thursday")
+//         break;
+//     case 5:
+//         document.write("Friday")
+//         break;
+//     case 6:
+//         document.write("Saturday")
+//         break;
+//     case 7:
+//         document.write("Sunday")
+//         break;
+//     default:
+//         document.write("Invalid day")
+//         break;
+        
+// }
+
+
+// let totMarks=300
+// let subj1=+prompt("Enter Subject 1 Marks")
+// let subj2=+prompt("Enter Subject 2 Marks")
+// let subj3=+prompt("Enter Subject 3 Marks")
+
+// let obtMarks=subj1+subj2+subj3
+
+
+
